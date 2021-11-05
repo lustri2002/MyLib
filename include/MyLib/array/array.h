8d6bd6ad56358b96e3069c801077d0ef8192e021
lustri2002 <lustri2002@gmail.com>
2021-11-05 23:34:57 +0100

    added insertion sort and fillarray

--- include/MyLib/array/array.h
+++ include/MyLib/array/array.h
@@ -1,6 +1,11 @@
 #ifndef MYLIB_ARRAY_H
 #define MYLIB_ARRAY_H
 
+#define in
+#define out
+#define inout
+
 void mylib_print(int v[], int n);
+void mylib_insertion_sort(inout int v[], in int n);
 
 #endif //MYLIB_ARRAY_H
--- src/array/array.c
+++ src/array/array.c
@@ -1,11 +1,37 @@
 #include <MyLib/array/array.h>
 
 #include <stdio.h>
+#include <ctime>
+#include <stdlib.h>
 
-void mylib_print(int v[], int n){
-    for(int i = 0; i < n; i++)
+void mylib_print(int v[], int n)
+{
+    for (int i = 0; i < n; i++)
     {
         printf("%d", v[i]);
     }
 }
 
+void mylib_insertion_sort(inout int v[], in int n)
+{
+    for (unsigned int i = 1; i < n; i++)
+    {
+        int value = v[i];
+        unsigned int j = i - 1;
+        while (j >= 0 && v[j] > value)
+        {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = value;
+    }
+}
+
+void mylib_fillarray(inout int v[], in int n, in int max)
+{
+    srand(time(NULL));
+    for(int i = 0; i < n; i++)
+    {
+        v[i] = rand()%max;
+    }
+}
